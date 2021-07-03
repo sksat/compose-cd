f4442a94233985432a7c0b06f4e65b13f2a62ee8
sksat <sksat@sksat.net>
2021-07-04 01:41:29 +0900

    こわれたためなおした (#11)
    * fix: add authorization in get_remote_image
    
    * add: test for get_remote_image()
    
    * fix: shellcheck
    
    * add: test on GitHub Actions
    
    * fix: rename VERSION to IMG_TAG(#8)
    
    * add: custom script(#5)
    
    * fix: shellcheck
    
    * fix: remote image null check
    
    * fix: shellcheck
    
    * fix: shellcheck
    
    * fix: silent curl
    
    * fix: tag
    
    * add: kuso-subdomain-adder to test
    
    * update: split manifest & check error
    
    * fix: shellcheck
    
    * update: version to 0.2.0

---
+++ test/get_remote.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd `dirname $0`
+
+source ../compose-cd
+
+function check_multi_registry(){
+	local image
+	local tag
+
+	local ghcr
+	local dockerhub
+
+	image=$1
+	tag=$2
+
+	echo -n "${image}:${tag} "
+
+	ghcr=$(get_remote_image "ghcr.io" ${image} ${tag})
+	dockerhub=$(get_remote_image "hub.docker.com" ${image} ${tag})
+
+	if [ "$ghcr" != "$dockerhub" ]; then
+		echo "error"
+		echo -e "\tghcr.io: ${ghcr}"
+		echo -e "\tDockerHub: ${dockerhub}"
+		exit 1
+	fi
+
+	echo "OK"
+}
+
+check_multi_registry "sksat/archbot-rs" "main"
+check_multi_registry "sksat/mc.yohane.su" "main"
+check_multi_registry "sksat/papermc-docker" "main"
+check_multi_registry "sksat/kuso-subdomain-adder" "master"
